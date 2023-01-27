30df9d2fecc9ef4a833b9bcb4c0d37d466e449b8
Eric Leonard <lenny2k12@gmail.com>
2023-01-27 21:28:57 +0000

    finished lab

--- src/app/strings/greet.spec.ts
+++ src/app/strings/greet.spec.ts
@@ -4,4 +4,9 @@ describe('greet', () => {
     it('it should include the name in the message', () =>{
         expect (greet('Frank')).toContain('Frank');
     });
+
+    //meant to fail //
+    it('it should not include name in message', () => {
+        expect (greet('Frank')).not.toContain('Frank');
+    })
 });
